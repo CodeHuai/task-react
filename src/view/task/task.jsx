15dac894b75d3d94836155904f0ab1bd23e9b292
andbreak <1622170503@qq.com>
2023-11-08 20:52:01 +0800

    处理样式

--- src/view/task/task.jsx
+++ src/view/task/task.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {Button} from 'antd'
+import './task.less'
 
 class Task extends React.Component {
   render() {
     return (
-        <div>123</div>
+        <div className="wrapper">
+          <div className="header">
+            <h2>TASK OA 任务管理系统</h2>
+            <Button type="primary">新增</Button>
+          </div>
+        </div>
     )
   }
 }
